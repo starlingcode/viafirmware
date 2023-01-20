ac49a75690548c156b92db6b42cd432fd42f36fb
Will Mitchell <liquidcitymotors@gmail.com>
2023-01-20 12:59:16 -0600

    Fix table description key typo

--- wavetables/table_edit_helper.py
+++ wavetables/table_edit_helper.py
@@ -6,7 +6,8 @@ with open('tables.json') as jsonfile:
     old_data = json.load(jsonfile)
     for thing in old_data:
         new_data[thing] = old_data[thing]
-        new_data[thing]['desription'] = "Describe meeeeeeeee"
+        new_data[thing].pop('desription')
+        new_data[thing]['description'] = "Describe meeeeeeeee"
 
 with open('tables.json', 'w') as jsonfile:
     json.dump(new_data, jsonfile)
