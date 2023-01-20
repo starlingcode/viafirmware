4686a2d70f098e52f025a9826cc0e5491fa8ae3c
Will Mitchell <liquidcitymotors@gmail.com>
2023-01-20 16:47:13 -0600

    Begin table tags

--- wavetables/table_edit_helper.py
+++ wavetables/table_edit_helper.py
@@ -4,10 +4,9 @@ new_data = {}
 
 with open('tables.json') as jsonfile:
     old_data = json.load(jsonfile)
+    new_data = old_data
     for thing in old_data:
-        new_data[thing] = old_data[thing]
-        new_data[thing].pop('desription')
-        new_data[thing]['description'] = "Describe meeeeeeeee"
+        old_data[thing]["tags"]
 
-with open('tables.json', 'w') as jsonfile:
-    json.dump(new_data, jsonfile)
+# with open('tables.json', 'w') as jsonfile:
+#     json.dump(old_data, jsonfile, indent=4)
